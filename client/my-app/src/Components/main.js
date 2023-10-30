78207f2861c0505ff5ddca64f2c190ccba18fabd
Ombito <alvinombito09@gmail.com>
2023-10-30 23:45:28 +0300

    Fetch pizza items

--- client/my-app/src/Components/main.js
+++ client/my-app/src/Components/main.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 function RestaurantList() {
   const [restaurants, setRestaurants] = useState([]);
+  const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
     fetch('http://127.0.0.1:5555/restaurants')
       .then((res) => res.json())
       .then((data) => setRestaurants(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);  
+    
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/pizzas')
+      .then((res) => res.json())
+      .then((data) => setPizzas(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
@@ -17,6 +25,17 @@ function RestaurantList() {
                 <li key={restaurant.id}>
                 <h2>Name: {restaurant.name}</h2>
                 <p>Address: {restaurant.address}</p>
+                </li>
+            ))}
+        </ol>
+        <br></br>
+        <br></br>
+        <h1>PIZZAS</h1>
+        <ol>
+            {pizzas.map((pizza) => (
+                <li key={pizza.id}>
+                <h2>Name: {pizza.name}</h2>
+                <p>Ingredients: {pizza.ingredients}</p>
                 </li>
             ))}
         </ol>
